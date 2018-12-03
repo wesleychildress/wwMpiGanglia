6406119177c9786a88519b4564dca8ff1cc4694b
WesleyChildress <wesleychildress@icloud.com>
2018-12-03 08:17:22 -0500

    scripts

--- install2.sh
+++ install2.sh
@@ -2,8 +2,13 @@
 
 # current directory for reference
 DIR=$( pwd )
+# masternode
 masternode=$(hostname)
-wwsh node new n0001 --hwaddr=$ --ipaddr=10.253.1.1
+
+# ask for non-root user
+read -p "Enter the MAC address of n0001: "  MAC
+wwsh node new n0001 --hwaddr=$MAC --ipaddr=10.253.1.1
+
 # ssh to n0001
 .$DIR/scripts/ssh1
 
@@ -12,3 +17,12 @@ wwsh node new n0001 --hwaddr=$ --ipaddr=10.253.1.1
 
 # pdsh as rooot
 .$DIR/scripts/ssh3
+
+# reboot nodes
+pdsh -R ssh -w n0001
+
+# install apache2
+apt-get -y install apache2
+
+# install php
+apt-get -y install php5 php5-mysql libapache2-mod-php5
